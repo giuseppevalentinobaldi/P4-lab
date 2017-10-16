d8028d0865f1364e7f7b61ed27c48a041104b31d
giuseppe <giuseppe@localhost>
2017-10-16 15:37:11 +0200

    fix run-p4 script

--- switch/run-p4.sh
+++ switch/run-p4.sh
@@ -2,9 +2,11 @@
 # Switch starter
 
 #echo Compilo p4
-#p4c-bmv2 --json /behavioral-model/targets/l2_switch/l2_switch.json /behavioral-model/targets/l2_switch/l2_switch.p4
+#p4c-bmv2 --json /behavioral-model/targets/l2_switch/l2_switch.json /behavioral-model/targets/l2_switch/l2_switch.p4 
 
 echo Starto il servizio
-./behavioral-model/targets/simple_router/simple_router -i 0@eth0 -i 1@eth1 --log-console /behavioral-model/targets/simple_router/ipv4_forward.json
+./behavioral-model/targets/simple_router/simple_router -i 0@eth0 -i 1@eth1 --log-console /behavioral-model/targets/simple_router/ipv4_forward.json --pcap &
 sleep 2
+echo "no Ready!!"
 /behavioral-model/tools/runtime_CLI.py --json /behavioral-model/targets/simple_router/ipv4_forward.json < /behavioral-model/targets/simple_router/commands.txt
+ echo "Ready!!"
