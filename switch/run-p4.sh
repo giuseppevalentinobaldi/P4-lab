041bc6a0eb681bcfec160c87c84df24324c9d372
giuseppe <giuseppe@localhost>
2017-10-16 18:14:37 +0200

    last commit

--- switch/run-p4.sh
+++ switch/run-p4.sh
@@ -6,7 +6,7 @@
 
 echo Starto il servizio
 ./behavioral-model/targets/simple_router/simple_router -i 0@eth0 -i 1@eth1 --log-console /behavioral-model/targets/simple_router/ipv4_forward.json --pcap &
-sleep 2
+sleep 30
 echo "no Ready!!"
 /behavioral-model/tools/runtime_CLI.py --json /behavioral-model/targets/simple_router/ipv4_forward.json < /behavioral-model/targets/simple_router/commands.txt
  echo "Ready!!"
