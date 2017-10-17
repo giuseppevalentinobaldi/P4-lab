b176eca8724b453f65839405bbc70fcda1295fba
giuseppe <giuseppe@localhost>
2017-10-17 17:46:19 +0200

    setup-lab_v.1

--- simple-lab/switch/run-p4.sh
+++ simple-lab/switch/run-p4.sh
@@ -5,8 +5,8 @@
 #p4c-bmv2 --json /behavioral-model/targets/l2_switch/l2_switch.json /behavioral-model/targets/l2_switch/l2_switch.p4 
 
 echo Starto il servizio
-./behavioral-model/targets/simple_router/simple_router -i 0@eth0 -i 1@eth1 --log-console /behavioral-model/targets/simple_router/ipv4_forward.json --pcap &
+./behavioral-model/targets/simple_router/simple_router -i 0@eth0 -i 1@eth1 --log-console /behavioral-model/targets/simple_router/simple_router.json --pcap &
 sleep 30
 echo "no Ready!!"
-/behavioral-model/tools/runtime_CLI.py --json /behavioral-model/targets/simple_router/ipv4_forward.json < /behavioral-model/targets/simple_router/commands.txt
+/behavioral-model/tools/runtime_CLI.py --json /behavioral-model/targets/simple_router/simple_router.json < /behavioral-model/targets/simple_router/commands.txt
  echo "Ready!!"
