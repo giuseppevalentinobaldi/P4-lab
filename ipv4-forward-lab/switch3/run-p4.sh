e5d8b574a813cff37d55b2dc7ebb232bd3430de6
giuseppe <giuseppe@localhost>
2017-10-18 23:56:46 +0200

    RC 1

--- ipv4-forward-lab/switch3/run-p4.sh
+++ ipv4-forward-lab/switch3/run-p4.sh
@@ -3,7 +3,7 @@
 
 #echo Compilo p4
 #p4c-bmv2 --json /behavioral-model/targets/l2_switch/l2_switch.json /behavioral-model/targets/l2_switch/l2_switch.p4 
-
+sysctl -w net.ipv4.ip_forward=0
 echo Starto il servizio
 ./behavioral-model/targets/simple_switch/simple_switch -i 0@eth0 -i 1@eth1 -i 2@eth2 --log-console /behavioral-model/targets/simple_router/ipv4_forward.json --pcap &
 sleep 10
