6864b45a514653c58239c21cd47a88644bde490d
giuseppe <giuseppe@localhost>
2017-10-23 11:04:14 +0200

    add socket tcp (stable)

--- lab-single-switch/client1/client.py
+++ lab-single-switch/client1/client.py
@@ -2,17 +2,27 @@
 
 import socket
 
-
 TCP_IP = '20.0.0.2'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
+
+print ("Client connect!")
+check = True
+i=0
+PACKET='packet'
+
+while check:
+    cond = input('Send packet flow? (y/n): ')
+    if cond.lower() == "y":
+        s.send(bytes(PACKET, 'utf-8'))
+        RECV= s.recv(BUFFER_SIZE)
+        print (RECV.decode(), i)
+        i=i+1
+    else:
+        check = False
+
 s.close()
-
-print "received data:", data
-
+print ("Client disconnect!")
