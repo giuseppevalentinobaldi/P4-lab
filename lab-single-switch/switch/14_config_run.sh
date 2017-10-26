f32ba611a1833205f09ff9cf78655aed67ffdd3f
giuseppe <giuseppe@localhost>
2017-10-26 18:38:31 +0200

    fix script

--- lab-single-switch/switch/14_config_run.sh
+++ lab-single-switch/switch/14_config_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Switch starter
-
+echo "> Compilo Programma P4_14!!"
+p4c-bmv2 --json /behavioral-model/targets/simple_router/ipv4_forward_P4_14.json /behavioral-model/targets/simple_router/ipv4_forward_P4_14.p4
 echo "> Configuro lo switch!!"
 sysctl -w net.ipv4.ip_forward=0
 /etc/init.d/procps restart
