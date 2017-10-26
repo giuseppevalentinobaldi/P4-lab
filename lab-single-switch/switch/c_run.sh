0c25bbc8462b48ae23d022cbb8c74e1473ef0008
giuseppe <giuseppe@localhost>
2017-10-26 18:24:29 +0200

    add new target in P4_14

---
+++ lab-single-switch/switch/c_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Switch starter
+
+echo "> Configuro lo switch!!"
+sysctl -w net.ipv4.ip_forward=0
+/etc/init.d/procps restart
+echo Starto il servizio
+./behavioral-model/targets/simple_router/simple_router -i 0@eth0 -i 1@eth1 --log-console /behavioral-model/targets/simple_router/ipv4_forward_P4_14.json --pcap &
+sleep 10
+echo "> no Ready!!"
+/behavioral-model/tools/runtime_CLI.py --json /behavioral-model/targets/simple_router/ipv4_forward_P4_14.json < /behavioral-model/targets/simple_router/commands_P4_14.txt
+echo "> Ready!!"
