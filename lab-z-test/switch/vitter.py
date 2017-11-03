e6c6644257aa2f54a2fa8da87e67787925b1484d
giuseppe <giuseppe@localhost>
2017-11-03 19:02:55 +0100

    vitter didactic version (for network)

--- lab-z-test/switch/vitter.py
+++ lab-z-test/switch/vitter.py
@@ -2,13 +2,17 @@ from scapy.all import *
 import logging, re, random, sys, math
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-N, n, t = 3, 0, 0
+N, n, t , V, s, num = 3, 0, 0, 0, 0, 0.0
 resevoir = [None] * N
+counter = 1
 
 def packet_callback_r(packet):
+    global counter
     if packet[TCP].payload:
         pkt = str(packet[TCP].payload)
         if packet[IP].dport == 80:
+            print("pacchetto #: " + `counter`)
+            counter = counter + 1
             r(packet)
 
 def r(packet):
@@ -16,6 +20,7 @@ def r(packet):
     if n < N:
         resevoir[n] = packet
         n = n + 1
+        print("------------------------ next step ------------------------")
     else:
         if t == 0:
             t = n + 1
@@ -26,20 +31,23 @@ def r(packet):
             resevoir[random_m] = packet
         for e in resevoir:
             print("\n{} ----HTTP----> {}:{}:\n{}".format(e[IP].src, e[IP].dst, e[IP].dport, str(bytes(e[TCP].payload))))
+        print("------------------------ next step ------------------------")
             
 def packet_callback_x(packet):
+    global counter
     if packet[TCP].payload:
         pkt = str(packet[TCP].payload)
         if packet[IP].dport == 80:
+            print("pacchetto #: " + `counter`)
+            counter = counter + 1
             x(packet)
-            
-V, s, num = 0, 0, 0.0
 
 def x(packet):
     global N, n, t, resevoir, V, s, num
     if n < N:
         resevoir[n] = packet
         n = n + 1
+        print("------------------------ next step ------------------------")
     else:
         if t == 0:
             t = n + 1
@@ -79,6 +87,7 @@ def x(packet):
             s = s - 1
         for e in resevoir:
             print("\n{} ----HTTP----> {}:{}:\n{}".format(e[IP].src, e[IP].dst, e[IP].dport, str(bytes(e[TCP].payload))))
+        print("------------------------ next step ------------------------")
 
 def main(argv):
     print ("Eseguo algoritmo " + (argv.capitalize()))
