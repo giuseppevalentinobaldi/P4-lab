0db7a3b8cdcf1b701b1004df50a6d3cbaee289ea
giuseppe <giuseppe@localhost>
2017-11-15 00:17:57 +0100

    fix lstart

--- lab-z-test/lstart.sh
+++ lab-z-test/lstart.sh
@@ -12,17 +12,21 @@ function ctrl_c() {
 function run(){
 	lstart
 	echo "> Run client configuration!!"
-	sudo docker exec netkit_1000_client /config_run.sh
+	sudo nohup docker exec netkit_1000_client /config_run.sh </dev/null &>/dev/null &
 	echo "> Run server configuration!!"
-	sudo docker exec netkit_1000_server /config_run.sh
+	sudo nohup docker exec netkit_1000_server /config_run.sh </dev/null &>/dev/null &
 	echo "> Run reservoir configuration!!"
-	sudo docker exec netkit_1000_reservoir /config_run.sh
+	sudo nohup docker exec netkit_1000_reservoir /config_run.sh </dev/null &>/dev/null &
 	echo "> Run switch configuration!!"
 	echo "1 - to run simple router copy to cpu"
 	echo "2 - to run simple switch r"
 	echo "3 - to run simple switch x"
 	read -p 'What do you want to do ?: ' dovar
-	sudo docker exec netkit_1000_switch /config_run.sh $dovar
+	sudo nohup docker exec netkit_1000_switch /config_run.sh $dovar </dev/null &>/dev/null &
+	sleep 10
+	echo "> ready!!"
+	echo "Press crtl + c for terminate lab"
+	wait
 }
 
 run
