b944c2512ed282b81ed978bd7709647b6bf5d446
giuseppe <giuseppe@localhost>
2017-11-15 12:27:35 +0100

    fix lstart

--- lab-vitter/lstart.sh
+++ lab-vitter/lstart.sh
@@ -20,9 +20,9 @@ function run(){
     echo "> Run reservoir configuration!!"
     nohup docker exec netkit_1000_reservoir /config_run.sh </dev/null &>/dev/null &
     echo "> Run switch configuration!!"
-    echo "1 - to run simple router copy to cpu"
-    echo "2 - to run simple switch r"
-    echo "3 - to run simple switch x"
+    echo "1 - to run copy to cpu"
+    echo "2 - to run r"
+    echo "3 - to run x"
     read -p 'What do you want to do ?: ' dovar
     nohup docker exec netkit_1000_switch /config_run.sh $dovar </dev/null &>/dev/null &
     sleep 15
