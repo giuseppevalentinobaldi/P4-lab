60c08c295de2fd325420c8d720c5d2948ce270be
giuseppe <giuseppe@localhost>
2017-11-20 10:32:29 +0100

    add timestamp

--- lab-vitter/reservoir/reservoir.py
+++ lab-vitter/reservoir/reservoir.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
 
 from scapy.all import *
-import Queue
+import Queue, datetime
 
 N = 3
 q = Queue.Queue(N)
+
 
 def print_resevoir():
     global q
     tmp = Queue.Queue(N)
     print("=====================================================")
     while not (q.empty()) :
-        pkt=q.get()
+        pkt = q.get()
         tmp.put(pkt)
-        print("\n{} ----HTTP----> {}:{}:\n{}".format(pkt[IP].src, pkt[IP].dst, pkt[IP].dport, str(bytes(pkt[TCP].payload))))
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        print("{}\n{} ----HTTP----> {}:{}:\n{}".format(st, pkt[IP].src, pkt[IP].dst, pkt[IP].dport, str(bytes(pkt[TCP].payload))))
     print("=====================================================")
-    q=tmp
+    q = tmp
+
 
 def resevoir(packet):
     global q
@@ -27,13 +30,16 @@ def resevoir(packet):
         q.put(packet)
         print_resevoir()
 
+
 def packet_callback(packet):
     if packet[TCP].payload:
         resevoir(packet)
 
+
 def main():
     print ("> Reservoir ready!!")
-    sniff(iface = "eth0", filter = "tcp", prn=packet_callback, store=0)
+    sniff(iface="eth0", filter="tcp", prn=packet_callback, store=0)
+
 
 if __name__ == "__main__":
    main()
--- lab-vitter/server/multithread_socket.py
+++ lab-vitter/server/multithread_socket.py
@@ -5,6 +5,7 @@ import threading
 
 def main():
     server = Server("20.0.0.2", 5005, 1024, 5, None, None, None)
+    print("> Server ready!!")
     while True:
         conn = server.accept_connection()
         threading.Thread(target=server.client_thread, args=(conn,),).start()
