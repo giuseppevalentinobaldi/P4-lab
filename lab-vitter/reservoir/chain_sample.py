7bd0e71be0b4897d4710628f14e782d6bfc7ad81
giuseppe <giuseppe@localhost>
2017-11-29 12:56:09 +0100

    soft fix chain_sample.py

--- lab-vitter/reservoir/chain_sample.py
+++ lab-vitter/reservoir/chain_sample.py
@@ -2,21 +2,21 @@
 
 from scapy.all import *
 from queue import Queue
-from cStringIO import StringIO
+#from cStringIO import StringIO
 import datetime, time, sys
 
 
-class Capturing(list):
-
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
-
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio  # free up some memory
-        sys.stdout = self._stdout
+#class Capturing(list):
+#
+#    def __enter__(self):
+#        self._stdout = sys.stdout
+#        sys.stdout = self._stringio = StringIO()
+#        return self
+#
+#    def __exit__(self, *args):
+#        self.extend(self._stringio.getvalue().splitlines())
+#        del self._stringio  # free up some memory
+#        sys.stdout = self._stdout
 
 
 class ChainSample():
