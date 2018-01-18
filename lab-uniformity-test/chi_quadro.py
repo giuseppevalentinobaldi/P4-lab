8a4ca8d2c177e9b6ed222efb12598a7fbc0774d6
Giuseppe Valentino Baldi <giuseppevalentinobaldi@gmail.com>
2018-01-18 11:30:37 +0100

    fix chi square

--- lab-uniformity-test/chi_quadro.py
+++ lab-uniformity-test/chi_quadro.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python3
+'''
+Random Sampling with a Reservoir
+JEFFREY SCOTT VITTER
+@see: http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.138.784&rep=rep1&type=pdf
+
+@authors: Giuseppe Valentino Baldi, Marco Lorini
+@contact: giuseppevalentinobaldi@gmail.com, marck_91@hotmail.it 
+@version: 1.0.0
+@license: MIT (http://opensource.org/licenses/MIT)
+'''
+
 import sys, csv
 from scipy.stats import chisquare
 
@@ -5,21 +17,26 @@ from scipy.stats import chisquare
 def main():
     with open('uniform.csv', newline='') as f:
         reader = csv.reader(f, delimiter=',', lineterminator="\n")
-        #threshold = 0
-        #hypothesis = 0
-        #pValue = 0.05
+        threshold = 0
+        hypothesis = 0
+        pValue = 0.05
+        transitory = input("Inserisci il tuo transitorio:")
+        W = int(transitory)
         for row in reader:
-            #threshold += 1
+            threshold += 1
             desired_array = [int(numeric_string) for numeric_string in row]
-            chi = chisquare(desired_array)
+            slice_obj = slice(W, len(desired_array))
+            array_without_transitory = desired_array[slice_obj]
+            chi = chisquare(array_without_transitory)
             print("chi-square: {} \t p-value: {}".format(chi[0], chi[1]))
-            #if chi[1] < 0.05:
-                #hypothesis += 1
-        #if hypothesis < threshold * 2 / 3:
-            #print("I accept the null hypothesis")
-        #else:
-            #print("I reject the null hypothesis")   
+            if chi[1] < 0.05:
+                hypothesis += 1
+        if hypothesis < threshold * 2 / 3:
+            print("I accept the null hypothesis")
+        else:
+            print("I reject the null hypothesis")   
         
 
 if __name__ == "__main__":
     main()
+
